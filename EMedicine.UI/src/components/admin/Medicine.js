78958e0fac8839d7862dd013a364a0097ff3cb70
rajaniraju <rajaniraju2003@gmail.com>
2024-02-29 16:18:17 -0600

    Edit now working.

--- EMedicine.UI/src/components/admin/Medicine.js
+++ EMedicine.UI/src/components/admin/Medicine.js
@@ -3,19 +3,29 @@ import AdminHeader from "./AdminHeader";
 import { useEffect, useState } from "react";
 import moment from "moment";
 import Table from "@mui/joy/Table";
-import { Modal, Stack } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 import Button from "@mui/joy/Button";
 import Input from "@mui/joy/Input";
 import axios from "axios";
 import ButtonGroup from "@mui/joy/ButtonGroup";
-import Box from "@mui/joy/Box";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import "./Medicine.css";
-import { WindowSharp } from "@mui/icons-material";
 
 export default function Medicine(props) {
+	const [id, setid] = useState(0);
+	const [editFeature, seteditFeature] = useState("false");
+	const [listToEdit, setlistToEdit] = useState({
+		name: "",
+		manufacturer: "",
+		unitPrice: 0,
+		discount: 0,
+		quantity: 0,
+		expDate: "",
+		imageUrl: "",
+		status: 0,
+	});
 	const [show, setShow] = useState("false");
 	const [medicineList, setMedicineList] = useState([]);
 	const [medicineObject, setMedicineObject] = useState({
@@ -39,15 +49,12 @@ export default function Medicine(props) {
 		const { name, value } = event.target;
 		setMedicineObject({ ...medicineObject, [name]: value });
 	};
+
 	//while submittting you are adding these values to db.
 	const handleSubmit = async () => {
 		console.log(medicineObject.ExpDate);
 		var inputdateString = medicineObject.ExpDate;
 		var isoDateString = moment(inputdateString.expDate).format();
-		//const [month, day, year] = inputdateString.split("/");
-		//const dateObject = new Date(`${year}-${month}-${day}`);
-		//var isoDateString = dateObject.toDateString();
-
 		const data = {
 			name: medicineObject.Name,
 			manufacturer: medicineObject.Manufacturer,
@@ -74,6 +81,7 @@ export default function Medicine(props) {
 	};
 
 	useEffect(() => {
+		console.log(editFeature);
 		const url = "https://localhost:7148/api/Admin/medicineList";
 		fetch(url)
 			.then((response) => response.json())
@@ -82,9 +90,61 @@ export default function Medicine(props) {
 
 		setShow(false);
 	}, []);
-	const getEdited = () => {
+
+	const handleEdit = (list, event) => {
 		console.log("to be edited");
+		seteditFeature(true);
+		setShow(true);
+		setid(list.id);
+		console.log(list);
+
+		setlistToEdit({
+			name: list.name,
+			manufacturer: list.manufacturer,
+			unitPrice: list.unitPrice,
+			discount: list.discount,
+			quantity: list.quantity,
+			expDate: list.expDate,
+			imageUrl: list.imageUrl,
+			status: list.status,
+		});
 	};
+	const handleEditChange = (event) => {
+		const { name, value } = event.target;
+		console.log(name, value);
+		// Update the corresponding field in listToEdit
+		setlistToEdit((prevState) => ({
+			...prevState,
+			[name]: value,
+		}));
+	};
+
+	const handleSubmitEdit = async () => {
+		console.log(listToEdit.expDate);
+		var inputdateString = listToEdit.expDate;
+		var isoDateString = moment(inputdateString).format();
+		console.log(id);
+		const data = {
+			id: id,
+			name: listToEdit.name ? listToEdit.name : "",
+			manufacturer: listToEdit.manufacturer ? listToEdit.manufacturer : "",
+			unitPrice: listToEdit.unitPrice ? listToEdit.unitPrice : 0,
+			discount: listToEdit.discount ? listToEdit.discount : 0,
+			quantity: listToEdit.quantity ? listToEdit.quantity : 0,
+			expDate: isoDateString,
+			imageUrl: listToEdit.imageUrl ? listToEdit.imageUrl : "",
+			status: listToEdit.status ? listToEdit.status : "",
+		};
+		console.log(data);
+		const url = "https://localhost:7148/api/Admin/updateMedicine";
+		try {
+			const response = await axios.post(url, data);
+			console.log(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	const handleDelete = async (id) => {
 		var inputdateString = "02-18-2029";
 		const [month, day, year] = inputdateString.split("/");
@@ -116,6 +176,7 @@ export default function Medicine(props) {
 		}
 		window.location.reload();
 	};
+
 	return (
 		<>
 			<AdminHeader name={props} />
@@ -158,7 +219,9 @@ export default function Medicine(props) {
 								</td>
 								<td key={index + Math.random()}>{list.status}</td>
 								<td key={index + Math.random()}>
-									<IconButton aria-label='edit' onClick={getEdited}>
+									<IconButton
+										aria-label='edit'
+										onClick={() => handleEdit(list)}>
 										<EditIcon sx={{ color: "blue" }} />
 									</IconButton>
 								</td>
@@ -174,135 +237,263 @@ export default function Medicine(props) {
 					})}
 				</tbody>
 			</Table>
-			<div className='button'>
-				<Button sx={{ sm: 1 /* margin top */ }} onClick={handleOpen}>
-					AddMedicine
-				</Button>
-				<Modal className='modal' show={show} onHide={handleCloseModal}>
-					<Modal.Header>
-						<Modal.Title className='modal-title'>
-							Add New Items
-						</Modal.Title>
-					</Modal.Header>
-					<Table
-						aria-label='basic table'
-						stripe='2n'
-						sx={(theme) => ({
-							'& th[scope="col"]': theme.variants.solid.neutral,
-							"& td": theme.variants.soft.neutral,
-						})}>
-						<thead>
-							<tr>
-								<th>Name</th>
-								<th>Manufacturer</th>
-								<th>UnitPrice</th>
-								<th>Discount</th>
-								<th>Quantity</th>
-								<th>ExpDate</th>
-								<th>ImageUrl</th>
-								<th>status</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>
-									<div>
-										<Input
-											name='Name'
-											type='text'
-											placeholder='Name'
-											value={medicineObject.Name}
-											onChange={handleChange}
-										/>
-									</div>
-								</td>
-								<td>
-									<div>
-										<Input
-											name='Manufacturer'
-											type='text'
-											placeholder='Manufacturer'
-											value={medicineObject.Manufacturer}
-											onChange={handleChange}
-										/>
-									</div>
-								</td>
-								<td>
-									<div>
-										<Input
-											name='UnitPrice'
-											type='text'
-											placeholder='UnitPrice'
-											value={medicineObject.UnitPrice}
-											onChange={handleChange}
-										/>
-									</div>
-								</td>
-								<td>
-									<div>
-										<Input
-											name='Discount'
-											type='text'
-											placeholder='Discount'
-											value={medicineObject.Discount}
-											onChange={handleChange}
-										/>
-									</div>
-								</td>
-								<td>
-									<div>
-										<Input
-											name='Quantity'
-											type='text'
-											placeholder='Quantity'
-											value={medicineObject.Quantity}
-											onChange={handleChange}
-										/>
-									</div>
-								</td>
-								<td>
-									<div>
-										<Input
-											name='ExpDate'
-											type='text'
-											placeholder='ExpDate'
-											value={medicineObject.ExpDate}
-											onChange={handleChange}
-										/>
-									</div>
-								</td>
-								<td>
-									<div>
-										<Input
-											name='ImageUrl'
-											type='text'
-											placeholder='ImageUrl'
-											value={medicineObject.ImageUrl}
-											onChange={handleChange}
-										/>
-									</div>
-								</td>
-								<td>
-									<div>
-										<Input
-											name='Status'
-											type='text'
-											placeholder='Status'
-											value={medicineObject.Status}
-											onChange={handleChange}
-										/>
-									</div>
-								</td>
-							</tr>
-						</tbody>
-					</Table>
-					<ButtonGroup aria-label='outlined primary button group'>
-						<Button onClick={handleCloseModal}>Close</Button>
+			{editFeature ? (
+				<div className='button'>
+					<Modal className='modal' show={show} onHide={handleCloseModal}>
+						<Modal.Header>
+							<Modal.Title className='modal-title'>
+								Edit Items
+							</Modal.Title>
+						</Modal.Header>
+						<Table
+							aria-label='basic table'
+							stripe='2n'
+							sx={(theme) => ({
+								'& th[scope="col"]': theme.variants.solid.neutral,
+								"& td": theme.variants.soft.neutral,
+							})}>
+							<thead>
+								<tr>
+									<th>Name</th>
+									<th>Manufacturer</th>
+									<th>UnitPrice</th>
+									<th>Discount</th>
+									<th>Quantity</th>
+									<th>ExpDate</th>
+									<th>ImageUrl</th>
+									<th>status</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										<div>
+											<Input
+												name='name'
+												type='text'
+												placeholder={listToEdit.name}
+												value={listToEdit.name}
+												onChange={handleEditChange}
+											/>
+										</div>
+									</td>
+									<td>
+										<div>
+											<Input
+												name='manufacturer'
+												type='text'
+												placeholder={listToEdit.manufacturer}
+												value={listToEdit.manufacturer}
+												onChange={handleEditChange}
+											/>
+										</div>
+									</td>
+									<td>
+										<div>
+											<Input
+												name='unitPrice'
+												type='number'
+												value={listToEdit.unitPrice}
+												onChange={handleEditChange}
+											/>
+										</div>
+									</td>
+									<td>
+										<div>
+											<Input
+												name='discount'
+												type='number'
+												value={listToEdit.discount}
+												onChange={handleEditChange}
+											/>
+										</div>
+									</td>
+									<td>
+										<div>
+											<Input
+												name='quantity'
+												type='number'
+												placeholder={listToEdit.quantity.toString()}
+												value={listToEdit.quantity}
+												onChange={handleEditChange}
+											/>
+										</div>
+									</td>
+									<td>
+										<div>
+											<Input
+												name='expDate'
+												type='text'
+												value={listToEdit.expDate}
+												onChange={handleEditChange}
+											/>
+										</div>
+									</td>
+									<td>
+										<div>
+											<Input
+												name='imageUrl'
+												type='text'
+												placeholder={listToEdit.imageUrl}
+												value={listToEdit.imageUrl}
+												onChange={handleEditChange}
+											/>
+										</div>
+									</td>
+									<td>
+										<div>
+											<Input
+												name='status'
+												type='text'
+												placeholder={listToEdit.status.toString()}
+												value={listToEdit.status}
+												onChange={handleEditChange}
+											/>
+										</div>
+									</td>
+								</tr>
+							</tbody>
+						</Table>
+						<ButtonGroup aria-label='outlined primary button group'>
+							<Button onClick={handleCloseModal}>Close</Button>
 
-						<Button onClick={handleSubmit}>Save Changes</Button>
-					</ButtonGroup>
-				</Modal>
-			</div>
+							<Button onClick={handleSubmitEdit}>
+								Save Edited Changes
+							</Button>
+						</ButtonGroup>
+					</Modal>
+				</div>
+			) : (
+				<div className='button'>
+					<Button sx={{ sm: 1 /* margin top */ }} onClick={handleOpen}>
+						AddMedicine
+					</Button>
+					<Modal className='modal' show={show} onHide={handleCloseModal}>
+						<Modal.Header>
+							<Modal.Title className='modal-title'>
+								Add New Items
+							</Modal.Title>
+						</Modal.Header>
+						<Table
+							aria-label='basic table'
+							stripe='2n'
+							sx={(theme) => ({
+								'& th[scope="col"]': theme.variants.solid.neutral,
+								"& td": theme.variants.soft.neutral,
+							})}>
+							<thead>
+								<tr>
+									<th>Name</th>
+									<th>Manufacturer</th>
+									<th>UnitPrice</th>
+									<th>Discount</th>
+									<th>Quantity</th>
+									<th>ExpDate</th>
+									<th>ImageUrl</th>
+									<th>status</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										<div>
+											<Input
+												name='Name'
+												type='text'
+												placeholder='Name'
+												value={medicineObject.Name}
+												onChange={handleChange}
+											/>
+										</div>
+									</td>
+									<td>
+										<div>
+											<Input
+												name='Manufacturer'
+												type='text'
+												placeholder='Manufacturer'
+												value={medicineObject.Manufacturer}
+												onChange={handleChange}
+											/>
+										</div>
+									</td>
+									<td>
+										<div>
+											<Input
+												name='UnitPrice'
+												type='text'
+												placeholder='UnitPrice'
+												value={medicineObject.UnitPrice}
+												onChange={handleChange}
+											/>
+										</div>
+									</td>
+									<td>
+										<div>
+											<Input
+												name='Discount'
+												type='text'
+												placeholder='Discount'
+												value={medicineObject.Discount}
+												onChange={handleChange}
+											/>
+										</div>
+									</td>
+									<td>
+										<div>
+											<Input
+												name='Quantity'
+												type='text'
+												placeholder='Quantity'
+												value={medicineObject.Quantity}
+												onChange={handleChange}
+											/>
+										</div>
+									</td>
+									<td>
+										<div>
+											<Input
+												name='ExpDate'
+												type='text'
+												placeholder='ExpDate'
+												value={medicineObject.ExpDate}
+												onChange={handleChange}
+											/>
+										</div>
+									</td>
+									<td>
+										<div>
+											<Input
+												name='ImageUrl'
+												type='text'
+												placeholder='ImageUrl'
+												value={medicineObject.ImageUrl}
+												onChange={handleChange}
+											/>
+										</div>
+									</td>
+									<td>
+										<div>
+											<Input
+												name='Status'
+												type='text'
+												placeholder='Status'
+												value={medicineObject.Status}
+												onChange={handleChange}
+											/>
+										</div>
+									</td>
+								</tr>
+							</tbody>
+						</Table>
+						<ButtonGroup aria-label='outlined primary button group'>
+							<Button onClick={handleCloseModal}>Close</Button>
+
+							<Button onClick={handleSubmit}>Save Changes</Button>
+						</ButtonGroup>
+					</Modal>
+				</div>
+			)}
 		</>
 	);
 }
